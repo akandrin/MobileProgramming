a4fc9968b50f1e756d76ab8c76f9c4d89f996d68
Alexey Kandrin <adkandrin@gmail.com>
2023-04-30 17:16:20 +0300

    lab2

---
+++ lab1/translations/Lab1-de.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Lab 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Increase value</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
